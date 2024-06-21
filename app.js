8fd02830837d29e3775aa342dfc63e83c417bb26
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-06-21 00:59:31 -0700

    $

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+#!/usr/bin/env node
+
+// Imports
 const { ApolloServer, gql } = require("apollo-server");
-
-const { Sequelize, DataTypes } = require("sequelize");
-
+const { Sequelize, DataTypes, Op } = require("sequelize");
 const GraphQLJSON = require("graphql-type-json");
-
 const { publicKeyToAddress, verifyCommit } = require("@albertiprotocol/sdk");
 
-const difficulty = process.env.ALBERTI_DIFFICULTY || 5;
+// Configurations
+const difficulty = parseInt(process.env.ALBERTI_DIFFICULTY) || 5;
+const port = parseInt(process.env.ALBERTI_PORT) || 4000;
 
+// Database setup
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./data/commits.db",
+  logging: false, // Disable logging for cleaner output
 });
 
+// Commit model definition
 const Commit = sequelize.define(
   "Commit",
   {
@@ -51,25 +56,27 @@ const Commit = sequelize.define(
   }
 );
 
+// Periodic task to delete old commits
 const deleteOldCommits = async () => {
-  const oneYearAgo = new Date();
-  oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+  try {
+    const threeMonthsAgo = new Date();
+    threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
 
-  await Commit.destroy({
-    where: {
-      updatedAt: {
-        [Sequelize.Op.lt]: oneYearAgo,
+    await Commit.destroy({
+      where: {
+        updatedAt: {
+          [Op.lt]: threeMonthsAgo,
+        },
       },
-    },
-  });
+    });
 
-  console.log("Old commits deleted successfully");
+    console.log("Old commits deleted successfully");
+  } catch (error) {
+    console.error("Error deleting old commits:", error);
+  }
 };
 
-// db init
-sequelize.sync();
-setInterval(deleteOldCommits, 24 * 60 * 60 * 1000);
-
+// GraphQL schema definition
 const typeDefs = gql`
   scalar JSON
 
@@ -114,13 +121,12 @@ const typeDefs = gql`
   }
 `;
 
+// GraphQL resolvers
 const resolvers = {
   JSON: GraphQLJSON,
   Query: {
     getRandomCommit: async () => {
-      return await Commit.findOne({
-        order: sequelize.random(),
-      });
+      return await Commit.findOne({ order: sequelize.random() });
     },
 
     getCommit: async (_, { signature }) => {
@@ -131,7 +137,7 @@ const resolvers = {
       const offset = (page - 1) * perPage;
       return await Commit.findAll({
         limit: perPage,
-        offset: offset,
+        offset,
         order: [["createdAt", "DESC"]],
       });
     },
@@ -139,23 +145,23 @@ const resolvers = {
     getCommitsByAddress: async (_, { address, page, perPage }) => {
       const offset = (page - 1) * perPage;
 
-      const datares = await Commit.findAll({
+      let commits = await Commit.findAll({
         where: { address },
         limit: perPage,
-        offset: offset,
+        offset,
         order: [["createdAt", "DESC"]],
       });
 
-      if (datares.length == 0) {
-        return await Commit.findAll({
+      if (commits.length === 0) {
+        commits = await Commit.findAll({
           where: { publicKey: address },
           limit: perPage,
-          offset: offset,
+          offset,
           order: [["createdAt", "DESC"]],
         });
       }
 
-      return datares;
+      return commits;
     },
 
     getAllAddresses: async () => {
@@ -169,26 +175,23 @@ const resolvers = {
 
     getServerInfo: async () => {
       const totalEntries = await Commit.count();
-
       const oldestEntry = await Commit.findOne({
         order: [["createdAt", "ASC"]],
       });
-
       const oldestEntryDate = oldestEntry
         ? oldestEntry.createdAt.toISOString()
         : null;
-
-      const uniqueAddressescount = await Commit.count({
+      const totalAddresses = await Commit.count({
         distinct: true,
         col: "address",
       });
 
       return {
-        difficulty: parseInt(difficulty),
+        difficulty,
         currentTime: new Date().toISOString(),
-        totalEntries: totalEntries,
-        totalAddresses: uniqueAddressescount,
-        oldestEntryDate: oldestEntryDate,
+        totalEntries,
+        totalAddresses,
+        oldestEntryDate,
       };
     },
   },
@@ -198,27 +201,32 @@ const resolvers = {
 
       if (!verifyCommit(args, difficulty)) {
         throw new Error(
-          "Difficulty not met, Current difficulty is " + difficulty
+          `Difficulty not met. Current difficulty is ${difficulty}`
         );
       }
 
-      return await Commit.create({ ...args, address });
+      try {
+        return await Commit.create({ ...args, address });
+      } catch (error) {
+        console.error("Error creating commit:", error);
+        throw new Error("Failed to create commit");
+      }
     },
   },
 };
 
+// Apollo Server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({ req }) => {
-    return { sequelize };
-  },
+  context: async () => ({ sequelize }),
 });
 
-server
-  .listen({
-    port: process.env.PORT || 4000,
-  })
-  .then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
+// Sync database and set interval for periodic tasks
+sequelize.sync();
+setInterval(deleteOldCommits, 24 * 60 * 60 * 1000);
+
+// Start the server
+server.listen({ port }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
